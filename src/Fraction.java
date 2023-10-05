844fc731aa3852372295b94b01c94309909cafba
Sebastien Marcil <sebz.marcil@gmail.com>
2023-10-04 22:37:41 -0400

    Finished

---
+++ src/Fraction.java
@@ -0,0 +1,50 @@
+public class Fraction implements Nombre<Fraction>{
+    private double numerateur;
+    private double denominateur;
+
+    public Fraction() {
+        numerateur = 0;
+        denominateur = 1;
+    }
+
+    public Fraction(double numerateur, double denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    @Override
+    public Fraction add(Fraction x) {
+        double numerateur = this.numerateur * x.denominateur + this.denominateur * x.numerateur;
+        double denominateur = this.denominateur * x.denominateur;
+        return new Fraction(numerateur, denominateur);
+    }
+
+    @Override
+    public Fraction sub(Fraction x) {
+        double numerateur = this.numerateur * x.denominateur - this.denominateur * x.numerateur;
+        double denominateur = this.denominateur * x.denominateur;
+        return new Fraction(numerateur, denominateur);
+    }
+
+    @Override
+    public Fraction mul(Fraction x) {
+        double numerateur = this.numerateur * x.numerateur;
+        double denominateur = this.denominateur * x.denominateur;
+        return new Fraction(numerateur, denominateur);
+    }
+
+    @Override
+    public Fraction div(Fraction x) {
+        double numerateur = this.numerateur * x.denominateur;
+        double denominateur = this.denominateur * x.numerateur;
+        return new Fraction(numerateur, denominateur);
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerateur=" + numerateur +
+                ", denominateur=" + denominateur +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,61 @@
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        NDouble double1 = new NDouble(6);
+        NDouble double2 = new NDouble(2);
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println(double1.add(double2));
+        System.out.println(double1.sub(double2));
+        System.out.println(double1.mul(double2));
+        System.out.println(double1.div(double2));
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println();
+
+        Fraction fraction1 = new Fraction(3,4);
+        Fraction fraction2 = new Fraction(1,3);
+
+        System.out.println(fraction1.add(fraction2));
+        System.out.println(fraction1.sub(fraction2));
+        System.out.println(fraction1.mul(fraction2));
+        System.out.println(fraction1.div(fraction2));
+
+        System.out.println();
+
+        int index = 0;
+        ArrayList<NDouble> nDoubles = new ArrayList<>();
+        nDoubles.add(new NDouble(index++));
+        nDoubles.add(new NDouble(index++));
+        nDoubles.add(new NDouble(index++));
+        nDoubles.add(new NDouble(index++));
+        nDoubles.add(new NDouble(index++));
+
+        System.out.println(somme(nDoubles));
+
+        System.out.println();
+
+
+        index = 1;
+        ArrayList<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction(index + 1, 2 * index++));
+        fractions.add(new Fraction(index + 1, 2 * index++));
+        fractions.add(new Fraction(index + 1, 2 * index++));
+        fractions.add(new Fraction(index + 1, 2 * index++));
+        fractions.add(new Fraction(index + 1, 2 * index++));
+
+        System.out.println(somme(fractions));
+    }
+
+    public static < N extends Nombre< N > > Nombre< N > somme( ArrayList< N > tableau ){
+        Nombre< N > resultat = null;
+        if(tableau != null && !tableau.isEmpty()) {
+            resultat = tableau.get(0);
+            for (int i=1; i< tableau.size(); i++) {
+                resultat = resultat.add(tableau.get(i));
+            }
         }
+        return resultat;
     }
 }
